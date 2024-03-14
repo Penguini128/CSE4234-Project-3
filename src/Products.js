2e739af5fd1f2f9ffd3f3add541a5ea98b851c7d
Haley <hamiltonh2021@my.fit.edu>
2024-03-14 12:28:28 -0400

    added json parsing to Product.json

--- src/Products.js
+++ src/Products.js
@@ -1,14 +1,52 @@
-import Product from "./Product";
+import React from 'react';
+import {useEffect, useState} from 'react';
+//export default Products;
+
 function Products() {
-    return (
-        <div className="product-table">
-            <ul>
-                <li><Product/></li>
-                <li><Product/></li>
-                <li><Product/></li>
-            </ul>
-        </div>
-    )
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [inventory, setInventory] = useState([]);
+
+    useEffect(() => {
+
+        fetch("products.json")
+            .then(response => response.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setInventory(result);
+                },
+
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [])
+
+    if (error) {
+        return <div> Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <div className="product-content">
+                <ul>
+                    {inventory.map(product => (
+                    <li key={product.id}>
+                        <h2>{product.title}</h2>
+                        <p>Price: ${product.price}</p>
+                        <p>Category: {product.category}</p>
+                        <p>Rating: {product.rating.rate}</p>
+                        <p>Inventory: ${product.inventory}</p>
+                        <p>Revenue: ????? </p>
+                        <p>Button: goes here</p>
+                    </li>
+                   ))}
+                </ul>
+            </div>
+        )
+    }
 }
 
 export default Products;
